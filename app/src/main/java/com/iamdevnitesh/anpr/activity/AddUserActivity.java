9251f43f2d941f33e85cb5d26b0d8c79777a9633
iamdevnitesh <i.am.dev.nitesh@gmail.com>
2022-11-24 18:27:41 +0530

    UI Elements enhanced

--- app/src/main/java/com/iamdevnitesh/anpr/activity/AddUserActivity.java
+++ app/src/main/java/com/iamdevnitesh/anpr/activity/AddUserActivity.java
@@ -1,9 +1,11 @@
 package com.iamdevnitesh.anpr.activity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
@@ -13,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.iamdevnitesh.anpr.R;
 import com.iamdevnitesh.anpr.databinding.ActivityAddUserBinding;
 
 public class AddUserActivity extends AppCompatActivity {
@@ -24,6 +27,12 @@ public class AddUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityAddUserBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Register User");
+        ColorDrawable colorDrawable = new ColorDrawable(getResources().getColor(R.color.ActionBar));
+        ab.setBackgroundDrawable(colorDrawable);
+
 
         mAuth = FirebaseAuth.getInstance();
 
